a95fd9f79a0469c51ca9da3e5e3ebefed7a314ac
david.mera <david.mera@certicamara.com>
2016-07-01 09:51:00 -0500

    Se configura ejecucion de prueba de integracion con maven

--- cubeSummation/src/test/java/com/mera/cubeSummation/service/CubseSummationServiceIntegrationTest.java
+++ cubeSummation/src/test/java/com/mera/cubeSummation/service/CubseSummationServiceIntegrationTest.java
@@ -34,12 +34,13 @@ import com.sun.jersey.api.client.WebResource;
  */
 public class CubseSummationServiceIntegrationTest {
 
-//	Uncomment the line below when you are running tests with JUnit
-	private static final String CONFIGURATION_FILE = "cube-summation-configuration.yml";
-	
-//  Uncomment the line below when you are running tests with maven
-//	private static final String CONFIGURATION_FILE = "../cube-summation-configuration.yml";
-	
+	// Uncomment the line below when you are running tests with JUnit
+	// private static final String CONFIGURATION_FILE =
+	// "cube-summation-configuration.yml";
+
+	// Uncomment the line below when you are running tests with maven
+	private static final String CONFIGURATION_FILE = "../cube-summation-configuration.yml";
+
 	@ClassRule
 	public static final DropwizardAppRule<Configuration> RULE = new DropwizardAppRule<Configuration>(
 			ApiEntryPoint.class, CONFIGURATION_FILE);
@@ -69,26 +70,28 @@ public class CubseSummationServiceIntegrationTest {
 	 */
 	@Test
 	public void testGetCubeSummation() {
-		
-		List<CubeSummationTestCase> cubeSummationTests = new ArrayList<>(); 
-		cubeSummationTests.add(new CubeSummationTestCase(4, 
-				new String[]{ "UPDATE 2 2 2 4", "QUERY 1 1 1 3 3 3", 
-				"UPDATE 1 1 1 23", "QUERY 2 2 2 4 4 4", "QUERY 1 1 1 3 3 3"}));
-		cubeSummationTests.add(new CubeSummationTestCase(2, 
-				new String[]{"UPDATE 2 2 2 1", "QUERY 1 1 1 1 1 1",
-		"QUERY 1 1 1 2 2 2", "QUERY 2 2 2 2 2 2"}));
-		CubeSummationRequest requestCubeSummation = new CubeSummationRequest(cubeSummationTests);
+
+		List<CubeSummationTestCase> cubeSummationTests = new ArrayList<>();
+		cubeSummationTests.add(new CubeSummationTestCase(4, new String[] {
+				"UPDATE 2 2 2 4", "QUERY 1 1 1 3 3 3", "UPDATE 1 1 1 23",
+				"QUERY 2 2 2 4 4 4", "QUERY 1 1 1 3 3 3" }));
+		cubeSummationTests.add(new CubeSummationTestCase(2, new String[] {
+				"UPDATE 2 2 2 1", "QUERY 1 1 1 1 1 1", "QUERY 1 1 1 2 2 2",
+				"QUERY 2 2 2 2 2 2" }));
+		CubeSummationRequest requestCubeSummation = new CubeSummationRequest(
+				cubeSummationTests);
 		WebResource resource = CLIENT.resource(CUBE_SUMMATION_URL);
 		ClientResponse response = resource
-				.type(MediaType.APPLICATION_JSON_TYPE)
-				.post(ClientResponse.class, requestCubeSummation);
+				.type(MediaType.APPLICATION_JSON_TYPE).post(
+						ClientResponse.class, requestCubeSummation);
 		int status = response.getStatus();
 		assertEquals(200, status);
 		String entity = response.getEntity(String.class);
 		assertFalse(StringUtils.isBlank(entity));
 		System.out.println(entity);
-		CubeSummationResponse responseCubeSummation = GSON.fromJson(entity, CubeSummationResponse.class);
+		CubeSummationResponse responseCubeSummation = GSON.fromJson(entity,
+				CubeSummationResponse.class);
 		assertNotNull(responseCubeSummation);
-		
+
 	}
 }
